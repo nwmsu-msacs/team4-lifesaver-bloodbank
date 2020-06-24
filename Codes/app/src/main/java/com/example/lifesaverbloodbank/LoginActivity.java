f4076f51a5ef3e0a335912fca081acf9df38c701
S537236 <s537236@nwmissouri.edu>
2020-06-23 22:08:46 -0500

    started working on login functionality

--- Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.example.lifesaverbloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginBTN;
+    private Button signupBTN;
+    private EditText emialET;
+    private EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emialET = (EditText) findViewById(R.id.emailET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
+        loginBTN = (Button) findViewById(R.id.loginBTN);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        signupBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            }
+        });
     }
 }
---
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/SignUpActivity.java
@@ -0,0 +1,4 @@
+package com.example.lifesaverbloodbank;
+
+public class SignUpActivity {
+}
