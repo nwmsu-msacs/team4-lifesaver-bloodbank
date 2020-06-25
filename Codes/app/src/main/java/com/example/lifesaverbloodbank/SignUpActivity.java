85e9767790010b24bb761472b5e3e2690fd3e1ed
S537236 <s537236@nwmissouri.edu>
2020-06-25 14:51:52 -0500

    added intent to sign in page from sign up page

--- Codes/app/src/main/java/com/example/lifesaverbloodbank/SignUpActivity.java
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/SignUpActivity.java
@@ -2,13 +2,28 @@ package com.example.lifesaverbloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private TextView signupTV;
+    private Button signinBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signupTV = findViewById(R.id.signupTV);
+        signinBTN = findViewById(R.id.signinBTN);
+        signinBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
