1dd791dd74a7e50f7ba82ac3206eb77ed1093f42
S537236 <s537236@nwmissouri.edu>
2020-06-25 11:11:08 -0500

    added navigation to forgot password

--- Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginBTN;
     private Button signupBTN;
     private EditText emialET;
     private EditText passwordET;
+    private TextView forgetTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         passwordET = (EditText) findViewById(R.id.passwordET);
         loginBTN = (Button) findViewById(R.id.loginBTN);
         signupBTN = (Button) findViewById(R.id.signupBTN);
+        forgetTV = (TextView) findViewById(R.id.forgetTV);
         loginBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +36,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        forgetTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, forgotPasswordActivity.class);
                 startActivity(intent);
             }
         });
