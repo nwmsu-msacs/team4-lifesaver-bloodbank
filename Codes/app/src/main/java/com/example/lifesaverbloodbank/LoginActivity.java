79c20494f0cf688605c74538a2229942649fc976
S537236 <s537236@nwmissouri.edu>
2020-06-24 21:20:19 -0500

    - removed libraries - updated code

--- Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/LoginActivity.java
@@ -33,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
