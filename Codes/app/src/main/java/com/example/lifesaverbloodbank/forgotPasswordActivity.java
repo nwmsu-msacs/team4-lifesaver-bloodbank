ad6cb885b15de250e6963d4c65b682e720824968
RaghunandanKumar <naishadamraghu@gmail.com>
2020-06-24 20:02:03 -0700

    added forgot password xml page

---
+++ Codes/app/src/main/java/com/example/lifesaverbloodbank/forgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.lifesaverbloodbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
