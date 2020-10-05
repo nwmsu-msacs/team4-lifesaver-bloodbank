2025663f23542bd41cc53503c818927e521d441c
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-10-04 22:42:48 -0500

    ResponseActivity.java on click listener added

--- Codes/app/src/main/java/com/example/myapplication/activities/ResponseActivity.java
+++ Codes/app/src/main/java/com/example/myapplication/activities/ResponseActivity.java
@@ -5,5 +5,23 @@
 
     }
 
+ private static class MyOnClickListener implements View.OnClickListener {
 
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForPosition(selectedItemPosition);
+
+        }
+    }
    
