383c578da201aa5ae89be9a5ca332130d6e77fbe
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-10-05 16:57:20 -0500

    ApplicationHolder code modified

--- Codes/app/src/main/java/com/example/myapplication/util/ApplicationHolder.java
+++ Codes/app/src/main/java/com/example/myapplication/util/ApplicationHolder.java
@@ -1,17 +1,10 @@
 package com.example.myapplication.util;
 
-/**
- * Created by aj1083 on 8/30/2016.
- */
-public class ApplicationHolder {
-    public static String userNames[]={"Anand","Naga babu","Suresh","Raghu","Avinash","Arun","swathi","shruthi","Shekar","Kajal"};
-    public static String userPhone[]={"9533320096","9498663556","9856237418","9884812113","9666100456","7812456395","9533320095","9865321245","8880123564","9645689745"};
-    public static String userState[]={"Andhra Pradesh","Telangana","Assam","Tamilnadu","Bhihar","Andhra Pradesh","Telangana","Assam","Tamilnadu","Bhihar"};
-    public static String userCity[]={"Vijayawada","Hyderabad","Guwahati","Chennai","Patna","Vijayawada","Hyderabad","Guwahati","Chennai","Patna"};
-    public static String userPincode[]={"521213","500045","500456","567435","523432","521213","500045","500456","567435","523432"};
-    public static String userBloogGroup[]={"O+","A","AB+","B+","AB-","O-","A-","B-","O+","AB+"};
 
-    public static String base_url="http://websitesdev.in/jjyothi/index.php/";
+public class ApplicationHolder {
+   
+
+    
     public static String login_url="Userlogin/login?";
     public static String registration_url="Userlogin/userRegistration?";
     public static String showProfile_url="Profile/showProfile?";
@@ -49,41 +42,6 @@ public class ApplicationHolder {
 
 
 
-   /* Registration
-
-    http://websitesdev.in/jjyothi/index.php/Userlogin/userRegistration?
-    donor_name=&emailid=&password=&dob=&gender=&bloodgroup=&how_often=&weight=&blooddonated
-    date=&res_phno=&address1=&city_name=&mob_no=
-
-    Login
-
-    http://websitesdev.in/jjyothi/index.php/Userlogin/login?username=&password=
-
-    Show User Profile:
-
-    http://websitesdev.in/jjyothi/index.php/Profile/showProfile?
-    email=suresh.pegadapelli@gmail.com
-
-    Update User Profile:
-
-    http://websitesdev.in/jjyothi/index.php/Profile/updateDonorprofile?
-    donor_name=&dob=&blood=&weight=&res_phno=&address1=&city_name=&mob_no=&userID=&image=
-
-    Add Request:
-
-    http://websitesdev.in/jjyothi/index.php/Bloodrequest/addRequest?
-    pname=&Bgroup=&pAge=&needon=&units=&mNumber=&lNumber=&hName=&location=&paddress=&purpos
-    e=&cName=&cEmail=
-
-    Edit Request:
-
-    http://websitesdev.in/jjyothi/index.php/Bloodrequest/editrequestformobile?
-    pname=&Bgroup=&pAge=&needon=&units=&mNumber=&lNumber=&hName=&location=&paddress=&purpos
-    e=&cName=&cEmail=&requestId=
-
-    Show All Request
-
-    http://websitesdev.in/jjyothi/index.php/Bloodrequest/showallrequest*/
-
+   
 
 }
