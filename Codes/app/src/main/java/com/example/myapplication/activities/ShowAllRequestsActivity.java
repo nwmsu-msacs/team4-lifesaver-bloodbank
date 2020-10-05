bc20a37a79620dc6b5d38951cbafd34418c95d33
gorantla96 <54447257+gorantla96@users.noreply.github.com>
2020-10-04 23:44:29 -0500

    Added code to all requests activity

--- Codes/app/src/main/java/com/example/myapplication/activities/ShowAllRequestsActivity.java
+++ Codes/app/src/main/java/com/example/myapplication/activities/ShowAllRequestsActivity.java
@@ -97,3 +97,23 @@ public class ShowAllRequestsActivity extends Activity implements View.OnClickLis
         }
 
     }
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForPosition(selectedItemPosition);
+
+        }
+    }
