c2278d38ed0ec573ab0b6db1f8641c89b7f6a538
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-10-05 01:03:14 -0500

    SharedPreference.java file added

---
+++ Codes/app/src/main/java/com/example/myapplication/util/SharedPreference.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Anand on 25/09/2016.
+ */
+public class SharedPreference {
+
+    Context c;
+    SharedPreferences sharedPref;
+    private final String SHARED_LIB = "DETAILS";
+    private final String MOBILE = "Mobile";
+
+    public SharedPreference(Context context) {
+        c = context;
+        sharedPref = c.getSharedPreferences("Preferences", Activity.MODE_PRIVATE);
+    }
+
+    public boolean setPrefValue(String tag, String tagValue) {
+        try {
+            SharedPreferences.Editor edit = sharedPref.edit();
+
+            edit.putString(tag,tagValue);
+            edit.commit();
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String getPrefValue(String tag) {
+        return sharedPref.getString(tag, "");
+    }
+
+
+}
