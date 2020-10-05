2ea481ee04db4151cd355f049b15170bd0db9f90
gorantla96 <54447257+gorantla96@users.noreply.github.com>
2020-10-04 23:41:51 -0500

    added code for blood request functionality

--- Codes/app/src/main/java/com/example/myapplication/activities/MyRequestsActivity.java
+++ Codes/app/src/main/java/com/example/myapplication/activities/MyRequestsActivity.java
@@ -232,3 +232,43 @@ public class MyRequestsActivity extends Activity implements View.OnClickListener
         }
 
     }
+
+    
+    @Override
+    public void onFailure(String failResponse) {
+
+    }
+
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForPosition(selectedItemPosition);
+
+        }
+    }
+
+    public void responseCount(String response){
+        int k= Integer.parseInt(response);
+        if(k>0) {
+            countNotifications.setVisibility(View.VISIBLE);
+            countNotifications.setText(response);
+        }
+        else{
+            countNotifications.setVisibility(View.GONE);
+        }
+    }
+}
+
